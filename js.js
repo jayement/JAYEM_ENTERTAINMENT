6164172a0dd321794a5477b5e06983f9dbc50dbd
jayement <jayementertainmentofc@gmail.com>
2026-05-21 16:57:43 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,37 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+
+    menuToggle.classList.toggle("active");
+    navbar.classList.toggle("active");
+
+});
+
+/* =========================
+SCROLL REVEAL
+========================= */
+
+const reveals = document.querySelectorAll(
+    ".reveal, .reveal-left, .reveal-right, .reveal-scale"
+);
+
+function revealOnScroll(){
+
+    const windowHeight = window.innerHeight;
+
+    reveals.forEach((element) => {
+
+        const top = element.getBoundingClientRect().top;
+
+        if(top < windowHeight - 100){
+            element.classList.add("active");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
